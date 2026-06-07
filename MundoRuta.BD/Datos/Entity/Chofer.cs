c1a2ee2d758415bf6f6fe612732ce45c1983d6f1
Eliana Neira <neiraeliana758@gmail.com>
2026-06-06 23:03:55 -0300

    Modelo Chofer Terminado

---
+++ MundoRuta.BD/Datos/Entity/Chofer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundoRuta.BD.Datos.Entity;
+
+public class Chofer : EntidadBase
+{
+
+    public required string Nombre { get; set; }
+    public  required string Apellido { get; set; }
+    public required string Licencia { get; set; }
+    public required bool Estado { get; set; }
+    public int IdPrestador { get; set; }
+  
+}
