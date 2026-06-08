3903ad389a30e132626f55b29f1c71c8cf235a44
santimartinez2 <santinomartinez2004@gmail.com>
2026-06-07 21:08:44 -0300

    tarea realizada

--- MundoRuta.BD/Datos/Entity/Vehiculo.cs
+++ MundoRuta.BD/Datos/Entity/Vehiculo.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace MundoRuta.BD.Datos.Entity
 {
-    internal class Vehiculo
+    public class Vehiculo : EntidadBase
     {
+        public string Patente { get; set; }
+        public string Marca { get; set; }
+        public string Licencia { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaFabricación { get; set; }
+        public int NumeroConductor { get; set; }
+        public string CaracteristicasConductor { get; set; }
+        public int IdChofer { get; set; }
+        public Chofer Chofer { get; set; }
+
     }
 }
