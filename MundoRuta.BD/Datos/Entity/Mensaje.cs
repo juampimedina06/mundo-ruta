d7d0f3208553bf342eb325c6805b5bd1b77b473e
Luciano Iacono <iaconojuanluciano@gmail.com>
2026-06-08 22:36:31 -0300

    modelo mensaje realizado

--- MundoRuta.BD/Datos/Entity/Mensaje.cs
+++ MundoRuta.BD/Datos/Entity/Mensaje.cs
@@ -6,8 +6,11 @@ namespace MundoRuta.BD.Datos.Entity
 {
     public class Mensaje : EntidadBase
     {
+        public int IdChat { get; set; }
+        public int? IdUsuario { get; set; }
+        public int? IdPrestador { get; set; }
         public string Contenido { get; set; }
-        public string EnviadoEn { get; set; }
+        public DateTime EnviadoEn { get; set; }
         public Chat Chat { get; set; }
         public Usuario Usuario { get; set; }
         public Prestador Prestador { get; set; }
