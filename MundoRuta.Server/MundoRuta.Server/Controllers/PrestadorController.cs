29df60529a6322317ccbbc70bf9396c8fe293162
Eliana Neira <neiraeliana758@gmail.com>
2026-08-19 20:04:50 -0300

    Cambios en la tabla Viaje, carpeta DTO y Controllers, modificaciones en el program

---
+++ MundoRuta.Server/MundoRuta.Server/Controllers/PrestadorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MundoRuta.BD.Datos;
+using MundoRuta.BD.Datos.Entity;
+using MundoRuta.Shared.DTO;
+using System.ComponentModel;
+
+
+namespace MundoRuta.Server.Controllers;
+
+[ApiController]
+[Route("api/prestador")]
+
+public class PrestadorController : ControllerBase
+{
+
+
+
+
+
+}
---
+++ MundoRuta.Shared/DTO/ResponderSolicitudDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundoRuta.Shared.DTO;
+
+public class ResponderSolicitudDTO
+{
+    public required string Accion { get; set; }
+    public int idChofer { get; set; }
+    public int idVehiculo { get; set; }
+}
