211d09d68a03dfb957332c98ea1243bf988f8b4c
santimartinez2 <santinomartinez2004@gmail.com>
2026-08-21 19:23:01 -0300

    Agregue catalogo publico de servicios

---
+++ MundoRuta.Shared/DTO/PrestadorPerfilPublicoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundoRuta.Shared.DTO;
+
+public class PrestadorPerfilPublicoDTO
+{
+    public int Id { get; set; }
+    public string RazonSocial { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+    public string Ciudad { get; set; }
+    public List<VehiculoDTO> Vehiculos { get; set; } = new List<VehiculoDTO>();
+}
---
+++ MundoRuta.Shared/DTO/VehiculoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MundoRuta.Shared.DTO;
+
+public class VehiculoDTO
+{
+    public int Id { get; set; }
+    public string Patente { get; set; }
+    public string Marca { get; set; }
+    public string Licencia { get; set; }
+    public string Estado { get; set; }
+}
