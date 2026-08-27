3ac0d5784a1fef497c2db1fd6c3abfb5d54069c4
Eliana Neira <neiraeliana758@gmail.com>
2026-08-26 22:02:09 -0300

    Sistema de Clificaciones

---
+++ MundoRuta.Server/MundoRuta.Server/Controllers/CalificacionesController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MundoRuta.BD.Datos;
+using MundoRuta.BD.Datos.Entity;
+using MundoRuta.Shared.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace MundoRuta.Server.Controllers
+{
+    [ApiController]
+    [Route("api/pasajero")]
+    public class CalificacionesController : ControllerBase
+    {
+
+        private readonly AppDbContext context;
+
+        public CalificacionesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpPost("calificaciones")]
+        public async  Task<ActionResult> Puntuacion([FromBody] CalificacionesDTO dto)
+        {
+            var viaje = await context.Viajes.FindAsync(dto.viajeId);
+
+            if (viaje == null)
+            {
+                return NotFound("Viaje no encontrado");
+            }
+
+            if(viaje.Estado!= "FINALIZADO")
+            {
+                return BadRequest("El viaje tiene que estar finalizado para poder calificarlo");
+            }
+
+            var calificacion = new Calificacion
+            {
+                IdViaje = dto.viajeId,
+                IdUsuario = dto.usuarioId,
+                IdPrestador = dto.prestadorId,
+                Puntaje = dto.puntaje,
+                Comentario = dto.comentario,
+            };
+
+            context.Calificaciones.Add(calificacion);
+            await context.SaveChangesAsync();
+
+            return Ok(calificacion);
+
+
+        }
+
+
+        [HttpGet("api/prestador/{IdPrestador}/calificaciones")]
+
+        public async Task<ActionResult> ListadoCalificaciones(int IdPrestador)
+        {
+            var lista = await context.Calificaciones.Where(c => c.IdPrestador== IdPrestador).ToListAsync();
+
+            double promedio = lista.Any() ? lista.Average(c => c.Puntaje) : 0.0;
+
+            return Ok(new
+            {
+                promedioCalificacion = promedio,   
+                totalReseñas = lista.Count,        
+                comentarios = lista               
+            });
+        }
+
+    }
+}
---
+++ MundoRuta.Shared/DTO/CalificacionesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MundoRuta.Shared.DTO;
+
+public class CalificacionesDTO
+{
+
+    public int viajeId {  get; set; }
+    public int usuarioId { get; set; }
+
+    public int prestadorId { get; set; }
+    public int puntaje { get; set; }
+    public string comentario { get; set; }
+}
